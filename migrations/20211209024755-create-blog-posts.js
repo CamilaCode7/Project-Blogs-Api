9a38814525dcf265e319a4aac6f3034587eb9bf6
CamilaCode7 <camilafabiabh9@gmail.com>
2021-12-08 23:50:20 -0300

    criando migrate de post

--- migrations/20211209024755-create-blog-posts.js
+++ migrations/20211209024755-create-blog-posts.js
@@ -1,31 +1,39 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('BlogPosts', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      displayName: {
-        allowNull: false,
-        type: Sequelize.STRING,
-      },
-      email: {
-        allowNull: false,
-        type: Sequelize.STRING,
-        unique: true
-      },
-      password: {
+      title: {
         allowNull: false,
         type: Sequelize.STRING
       },
-      image: {
-        type: Sequelize.STRING
+      content: {
+        type: Sequelize.STRING,
+        allowNull: false,
       },
-    })
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      published: {
+        type: Sequelize.DATE,
+      },
+      updated: {
+        type: Sequelize.DATE,
+      },
+    });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('BlogPosts');
   }
 };

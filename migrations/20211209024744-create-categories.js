b86290c3cb0613308f6995679518b60fbc1557c3
CamilaCode7 <camilafabiabh9@gmail.com>
2021-12-09 00:12:11 -0300

    adicionando migrate correta

--- migrations/20211209024744-create-categories.js
+++ migrations/20211209024744-create-categories.js
@@ -1,31 +1,20 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('Categories', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      displayName: {
+      name: {
         allowNull: false,
         type: Sequelize.STRING,
       },
-      email: {
-        allowNull: false,
-        type: Sequelize.STRING,
-        unique: true
-      },
-      password: {
-        allowNull: false,
-        type: Sequelize.STRING
-      },
-      image: {
-        type: Sequelize.STRING
-      },
-    })
+    });
   },
+
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('Categories');
   }
 };

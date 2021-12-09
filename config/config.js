3b451a196d892d6b183f02d9dff485bc146da77d
CamilaCode7 <camilafabiabh9@gmail.com>
2021-12-09 00:13:03 -0300

    update

--- config/config.js
+++ config/config.js
@@ -1,24 +1,24 @@
-require('dotenv').config().
+require('dotenv').config();
 
 module.exports = {
   development: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: 'blogs_api',
+    database: 'blogs_apis',
     host: process.env.HOSTNAME,
     dialect: 'mysql',
   },
   test: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: 'blogs_api',
+    database: 'blogs_apis',
     host: process.env.HOSTNAME,
     dialect: 'mysql',
   },
   production: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: 'blogs_api',
+    database: 'blogs_apis',
     host: process.env.HOSTNAME,
     dialect: 'mysql',
   },
